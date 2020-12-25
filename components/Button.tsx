b011fe3a9b42555bc603ce977081bafa00b8dde9
ta93imtk-wsl-ubuntu <ta93.imataku@me.com>
2020-12-26 03:20:27 +0900

    I: formatter

--- components/Button.tsx
+++ components/Button.tsx
@@ -26,39 +26,17 @@ export interface ButtonProps {
 /**
  * Primary UI component for user interaction
  */
-export const Button: React.FC<ButtonProps> = ({
-    primary = false,
-    size = "medium",
-    backgroundColor,
-    label,
-    ...props
-}) => {
+export const Button: React.FC<ButtonProps> = ({ primary = false, size = "medium", backgroundColor, label, ...props }) => {
     const baseButton = "rounded-full font-bold";
-    const sizeMode =
-        size === "small"
-            ? "py-1.5 px-4 text-xs"
-            : size === "medium"
-            ? "py-2 px-5 text-sm"
-            : size === "large"
-            ? "py-3 px-6 text-base"
-            : "";
+    const sizeMode = size === "small" ? "py-1.5 px-4 text-xs" : size === "medium" ? "py-2 px-5 text-sm" : size === "large" ? "py-3 px-6 text-base" : "";
     return primary ? (
         <div>
-            <button
-                type="button"
-                className={`text-white bg-blue-450 ${baseButton} ${sizeMode}`}
-                {...props}
-            >
+            <button type="button" className={`text-white bg-blue-450 ${baseButton} ${sizeMode}`} {...props}>
                 {label}
             </button>
         </div>
     ) : (
-        <button
-            type="button"
-            className={`text-gray-600 bg-transparent shadow-inner ${baseButton} ${sizeMode}`}
-            style={{ backgroundColor }}
-            {...props}
-        >
+        <button type="button" className={`text-gray-600 bg-transparent shadow-inner ${baseButton} ${sizeMode}`} style={{ backgroundColor }} {...props}>
             {label}
         </button>
     );
--- pages/index.tsx
+++ pages/index.tsx
@@ -13,40 +13,25 @@ export default function Home() {
                 <h1 className={styles.title}>
                     Welcome to <a href="https://nextjs.org">Next.js!</a>
                 </h1>
-                <p className="text-3xl text-red-500 font-bold">
-                    Hello Tailwind
-                </p>
+                <p className="text-3xl text-red-500 font-bold">Hello Tailwind</p>
                 <p className={styles.description}>
-                    Get started by editing{" "}
-                    <code className={styles.code}>pages/index.js</code>
+                    Get started by editing <code className={styles.code}>pages/index.js</code>
                 </p>
 
                 <div className={styles.grid}>
                     <a href="https://nextjs.org/docs" className={styles.card}>
                         <h3>Documentation &rarr;</h3>
-                        <p>
-                            Find in-depth information about Next.js features and
-                            API.
-                        </p>
+                        <p>Find in-depth information about Next.js features and API.</p>
                     </a>
 
                     <a href="https://nextjs.org/learn" className={styles.card}>
                         <h3>Learn &rarr;</h3>
-                        <p>
-                            Learn about Next.js in an interactive course with
-                            quizzes!
-                        </p>
+                        <p>Learn about Next.js in an interactive course with quizzes!</p>
                     </a>
 
-                    <a
-                        href="https://github.com/vercel/next.js/tree/master/examples"
-                        className={styles.card}
-                    >
+                    <a href="https://github.com/vercel/next.js/tree/master/examples" className={styles.card}>
                         <h3>Examples &rarr;</h3>
-                        <p>
-                            Discover and deploy boilerplate example Next.js
-                            projects.
-                        </p>
+                        <p>Discover and deploy boilerplate example Next.js projects.</p>
                     </a>
 
                     <a
@@ -54,10 +39,7 @@ export default function Home() {
                         className={styles.card}
                     >
                         <h3>Deploy &rarr;</h3>
-                        <p>
-                            Instantly deploy your Next.js site to a public URL
-                            with Vercel.
-                        </p>
+                        <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
                     </a>
                 </div>
             </main>
@@ -68,12 +50,7 @@ export default function Home() {
                     target="_blank"
                     rel="noopener noreferrer"
                 >
-                    Powered by{" "}
-                    <img
-                        src="/vercel.svg"
-                        alt="Vercel Logo"
-                        className={styles.logo}
-                    />
+                    Powered by <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
                 </a>
             </footer>
         </div>
